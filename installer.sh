5ca026cd032871655a0242fe93ac163ac67506ab
Nick <nicholascgilpin@users.noreply.github.com>
2023-01-11 18:10:16 -0600

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,15 @@
+    #!/bin/bash
+    
+#     Description:
+#     The installer.sh script provides common installation tools and information needed the other install scripts.
+#     The generator appends the other installers to this script. 
+    
+#     Tell bash to fail visibly.
+    set -euo pipefail
+    IFS=$'\n\t'
+    
+# Detect system information
+
+# Install common installation tools
+
+# Generated section
